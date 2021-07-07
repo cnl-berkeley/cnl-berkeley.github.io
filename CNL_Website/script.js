29b7283fb336c0d2a9e2001e6a33c88c0178eaa3
Ben Parker <anotherbenparker@gmail.com>
2021-07-07 10:09:30 -0700

    Off-site host

--- CNL_Website/script.js
+++
@@ -1,9 +0,0 @@
-const toggleButton =document.getElementsByClassName('toggle-button')[0]
-const navbarLinks= document.getElementsByClassName('nav-barlinks')[0]
-
-
-toggleButton.addEventListenter('click',() => {
-	navbarLinks.classList.toggle('active') /* if the active class exists it will add it*/
-})
-
-toggleButton()
