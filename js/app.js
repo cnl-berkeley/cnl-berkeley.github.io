c321f35d7b3c6306b1d6acf1e9fb5ea1bf256eda
Maxwell Chen <maxhchen@gmail.com>
2021-08-11 17:01:08 -0700

    Fix navigation issues on mobile

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ const navSlide = () => {
 	const burger = document.querySelector('.burger');
 	const nav = document.querySelector('.nav-links');
 	const navLinks=document.querySelectorAll('.nav-links li');
-
+	
 	burger.addEventListener('click', ()=>{
 		//Toggle Nav
+
 		nav.classList.toggle('nav-active');
 
 		//Animate Links
